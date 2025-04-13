14a6c61bb80eef02d06a9a9ab3c4b205154a1348
Aziz Sobirov <azizsobirovhazarasp@gmail.com>
2025-04-13 20:17:18 +0500

    Updates styles for Cycle block, adds Cycle block, and refactors Text component styling
    Adjusts padding for Trust block content
    Exports Cycle block in index
    Updates font sizes and styles in Text component

--- src/blocks/index.ts
+++ src/blocks/index.ts
@@ -1,3 +1,4 @@
+export { default as Cycle } from "./Cycle.astro";
 export { default as Hero } from "./Hero.astro";
 export { default as Stats } from "./Stats.astro";
 export { default as Trust } from "./Trust.astro";
